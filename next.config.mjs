7da2628e91e4538c4f81e28b7a970392704189c0
박지혜 <parkjihye@bagjihyeui-MacBookAir.local>
2024-10-17 23:34:50 +0900

    push

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    basePath: "/todo-list",
+  // next 이미지 최적화 옵션 off
+  images: {
+    unoptimized: true
+  }
+};
 
 export default nextConfig;
